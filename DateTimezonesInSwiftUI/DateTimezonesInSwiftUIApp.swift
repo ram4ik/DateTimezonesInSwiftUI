2dd0959841acfd7268a3d2309dec7734211a5e30
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-10 15:24:04 +0200

    Initial Commit

---
+++ DateTimezonesInSwiftUI/DateTimezonesInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateTimezonesInSwiftUIApp.swift
+//  DateTimezonesInSwiftUI
+//
+//  Created by test on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DateTimezonesInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
