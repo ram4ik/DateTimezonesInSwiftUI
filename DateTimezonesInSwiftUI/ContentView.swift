63341214f02c5fbbe6ac04666b0118e01877fd45
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-01-10 17:13:03 +0200

    init

--- DateTimezonesInSwiftUI/ContentView.swift
+++ DateTimezonesInSwiftUI/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var today = Date()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            Text("\(today)")
+                .padding()
+            
+            Text("\(today.formatted()) \(TimeZone.current.abbreviation() ?? "-")")
         }
         .padding()
     }
